cd6ef453520c2260238bd59b2c842497c4fa5191
JulianPichler1 <julian.pichler@students.bs-linz2.ac.at>
2022-02-17 20:12:55 +0100

    stand 20:12
    nur für mich sollte ich jetzt was kaputt machen

--- src/ResetPasswordServer.php
+++ src/ResetPasswordServer.php
@@ -17,20 +17,22 @@
     //connect to dbs
     $db = mysqli_connect('localhost', 'root', 'test1234', 'myarchery') or die("could't connect to database");
 
-    //Reset Password  --------------------------------------------------------------------------------------------------------
+    //start---------------------------------------------------------------------------------------------------------------------------------------------------------------
     //get inserted value
     $tmpemail = $_POST["email"];
     
     //check if we get some
     if(empty($tmpemail)) {array_push($errors, "E-Mail is required!");}
 
+
     //random Password
     for ($i = 0; $i < 10; $i++) {
         $randompassword .= $characters[rand(0, $charactersLength - 1)];
     }
-
+    //encrypt
     $randomencryptpassword = hash('sha256', $randompassword);
     
+
     //get use_id
     $user_check_query = "SELECT use_id FROM user WHERE email = '$tmpemail';";
     $results = mysqli_query($db, $user_check_query);
@@ -38,17 +40,18 @@
     
     $tmpuserid = $db_output['use_id'];
 
+    
+    //update tables
     //update untildate into password_history
-    $update_password_history = "UPDATE password_history SET Untildate = NOW() AND active = 0 WHERE use_id = '$tmpuserid' AND active = 1;";
+    $update_password_history = "UPDATE password_history SET untildate = NOW(), active = 0 WHERE use_id = '$tmpuserid' AND active = 1;";
     mysqli_query($db, $update_password_history);
-
     //insert new entry for user in password_history
     $insertquery_password_history = "INSERT INTO password_history (password, fromdate, untildate, use_id) VALUES ('$randomencryptpassword', NOW(), DATE_ADD(NOW(), INTERVAL 20 MINUTE), '$tmpuserid');";
     mysqli_query($db, $insertquery_password_history);
-    
     //insert new pw in user
     $insertquery_user = "UPDATE user SET password = '$randomencryptpassword' WHERE use_id = '$tmpuserid';";
     mysqli_query($db, $insertquery_user);
+   
 
     //send mail
     $to_email = "julian.pichler4@gmail.com";
