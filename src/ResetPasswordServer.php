6515ab20836352d463b167c1463cc0f33253b03b
JulianPichler1 <julian.pichler@students.bs-linz2.ac.at>
2022-02-18 11:32:57 +0100

    yee(fehlerhaft)

--- src/ResetPasswordServer.php
+++ src/ResetPasswordServer.php
@@ -9,38 +9,26 @@
     $tmpuntildate = "";
     $tmpactualdate = "";
     //for random PW
+    $tmpoldpassword = "";
     $randomencryptpassword = "";
     $randompassword = "";
     $characters = '.-0123456789abc.-defghijklm.-nopqrstuvwx.-yzABCDEFGHIJKLM.-NOPQRSTUVWXYZ.-';
     $charactersLength = strlen($characters);
 
-    $errors = array();
+    $message = array();
+
 
     //connect to dbs
     $db = mysqli_connect('localhost', 'root', 'test1234', 'myarchery') or die("could't connect to database");
 
+
     //start---------------------------------------------------------------------------------------------------------------------------------------------------------------
     //get inserted value
     $tmpemail = $_POST["email"];
-    
-    //check if we get some
-    if(empty($tmpemail)) {array_push($errors, "E-Mail is required!");}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> parent of 0f1e790 (changed reset pw)
-    if(count($errors) == 0 ) {
-        
-    } else {
-        include('Errors.php');
-        die();
-    }
-<<<<<<< HEAD
->>>>>>> parent of 0f1e790 (changed reset pw)
-=======
->>>>>>> parent of 0f1e790 (changed reset pw)
+    //check if we get some
+    if(empty($tmpemail)) {array_push($message, "E-Mail is required!");}
+
 
     //random Password
     for ($i = 0; $i < 10; $i++) {
@@ -48,8 +36,8 @@
     }
     //encrypt
     $randomencryptpassword = hash('sha256', $randompassword);
-    
 
+    
     //get use_id
     $user_check_query = "SELECT use_id FROM user WHERE email = '$tmpemail';";
     $results = mysqli_query($db, $user_check_query);
@@ -60,75 +48,37 @@
 
     //update tables
     //update untildate into password_history
-    $update_password_history = "UPDATE password_history SET untildate = NOW(), active = 0 WHERE use_id = '$tmpuserid' AND active = 1;";
+    $update_password_history = "UPDATE password_history SET untildate = NOW(), is_active = 0 WHERE use_id = '$tmpuserid' AND is_active = 1;";
     mysqli_query($db, $update_password_history);
     //insert new entry for user in password_history
-<<<<<<< HEAD
-<<<<<<< HEAD
-    $insertquery_password_history = "INSERT INTO password_history (password, fromdate, untildate, use_id) VALUES ('$randomencryptpassword', NOW(), DATE_ADD(NOW(), INTERVAL 20 MINUTE), '$tmpuserid');";
-=======
-    $insertquery_password_history = "INSERT INTO password_history (password, fromdate, untildate, is_temp, use_id) VALUES ('$randomencryptpassword', NOW(), DATE_ADD(NOW(), INTERVAL 2 MINUTE), '1', '$tmpuserid');";
->>>>>>> parent of 0f1e790 (changed reset pw)
-=======
-    $insertquery_password_history = "INSERT INTO password_history (password, fromdate, untildate, is_temp, use_id) VALUES ('$randomencryptpassword', NOW(), DATE_ADD(NOW(), INTERVAL 2 MINUTE), '1', '$tmpuserid');";
->>>>>>> parent of 0f1e790 (changed reset pw)
+    $insertquery_password_history = "INSERT INTO password_history (password, fromdate, use_id) VALUES ('$randomencryptpassword', NOW(), '$tmpuserid');";
     mysqli_query($db, $insertquery_password_history);
     //insert new pw in user
     $insertquery_user = "UPDATE user SET password = '$randomencryptpassword' WHERE use_id = '$tmpuserid';";
     mysqli_query($db, $insertquery_user);
-   
+
 
     //send mail
     $to_email = "julian.pichler4@gmail.com";
     $subject = "New MyArchery password!";
     $body = "Hi, This is your new password: \"$randompassword\" !";
     $headers = "From: myarchery.bslinz2@gmail.com";
-<<<<<<< HEAD
-    
-    if (mail($to_email, $subject, $body, $headers)) {
-<<<<<<< HEAD
-        echo "Email successfully sent to $to_email...";
-    } else {
-        echo "Email sending failed!";
-=======
-        echo "Email successfully sent to $to_email..." . '<br>';
-    } else {
-        echo "Email sending failed!" . '<br>';
->>>>>>> parent of 0f1e790 (changed reset pw)
-    }
-    
-    $i = 0;
-    while ($i == 0) {
-        //get untildate
-<<<<<<< HEAD
-        $get_untildate_query = "SELECT untildate FROM password_history WHERE use_id = '2' AND active = 1;";
-        $results = mysqli_query($db, $get_untildate_query);
-        $db_output = mysqli_fetch_assoc($results);
-        
-        $tmpuntildate = $db_output['use_id'];
-=======
-=======
-    
+
     if (mail($to_email, $subject, $body, $headers)) {
         echo "Email successfully sent to $to_email..." . '<br>';
     } else {
         echo "Email sending failed!" . '<br>';
     }
-    
+
     $i = 0;
     while ($i == 0) {
         //get untildate
->>>>>>> parent of 0f1e790 (changed reset pw)
         $get_untildate_query = "SELECT untildate FROM password_history WHERE use_id = '$tmpuserid' AND is_active = 1;";
         $results = mysqli_query($db, $get_untildate_query);
         $db_output = mysqli_fetch_assoc($results);
-        
+
         $tmpuntildate = $db_output['untildate'];
-<<<<<<< HEAD
->>>>>>> parent of 0f1e790 (changed reset pw)
-=======
->>>>>>> parent of 0f1e790 (changed reset pw)
-        
+
         //get actual date
         date_default_timezone_set("Europe/Vienna");
         $tmpactualdate = date("Y-m-d") . " " . date("H:i:s");
@@ -136,38 +86,20 @@
         //is untildate over?
         if ($tmpuntildate > $tmpactualdate) {
             sleep(60);
-<<<<<<< HEAD
-<<<<<<< HEAD
-        } 
-
-        
-    }
-
-
-
-
-    //check every min if password is expired
-    //get fromdate
-    /*
-    $user_check_query = "SELECT use_id FROM user WHERE email = '$tmpemail';";
-    $results = mysqli_query($db, $user_check_query);
-    $db_output = mysqli_fetch_assoc($results);
-    
-    $tmpuserid = $db_output['use_id'];
-    */
-
-=======
         } else {
             $i++;
         }
     }
 
-    
+    $_SESSION['to_email'] = "julian.pichler4@gmail.com";
+    $_SESSION['subject'] = "New MyArchery password!";
+    $_SESSION['emailbody'] = "Hi, This is your new password: \"$randompassword\" !";
+
     //get last pw from password_history
     $get_untildate_query = "SELECT password FROM password_history WHERE use_id = '$tmpuserid' AND is_temp = 0 AND is_active = 0 ORDER BY phy_id DESC LIMIT 1;";
     $results = mysqli_query($db, $get_untildate_query);
     $db_output = mysqli_fetch_assoc($results);
-    
+
     $tmpoldpassword = $db_output['password'];
 
 
@@ -181,6 +113,7 @@
     //insert old pw in user
     $insertquery_user = "UPDATE user SET password = '$tmpoldpassword' WHERE use_id = '$tmpuserid';";
     mysqli_query($db, $insertquery_user);
+    include('SendMail.php');
 
 
     //send mail for test
@@ -188,52 +121,17 @@
     $subject = "We changed you pw back!";
     $body = "Sheeeh wieder altes pw in DB!";
     $headers = "From: myarchery.bslinz2@gmail.com";
-    
+
     if (mail($to_email, $subject, $body, $headers)) {
         echo "Email successfully sent to $to_email..." . '<br>';
+    //pushin errors/msg's
+    if(count($message) == 0 ) {
+
     } else {
         echo "Email sending failed!" . '<br>';
+        include('Message.php');
+        die();
     }
 
->>>>>>> parent of 0f1e790 (changed reset pw)
-=======
-        } else {
-            $i++;
-        }
-    }
 
-    
-    //get last pw from password_history
-    $get_untildate_query = "SELECT password FROM password_history WHERE use_id = '$tmpuserid' AND is_temp = 0 AND is_active = 0 ORDER BY phy_id DESC LIMIT 1;";
-    $results = mysqli_query($db, $get_untildate_query);
-    $db_output = mysqli_fetch_assoc($results);
-    
-    $tmpoldpassword = $db_output['password'];
-
-
-    //update tables (now he inserts the previous password)
-    //update untildate into password_history
-    $update_password_history = "UPDATE password_history SET untildate = NOW(), is_active = 0 WHERE use_id = '$tmpuserid' AND is_active = 1;";
-    mysqli_query($db, $update_password_history);
-    //insert new entry for user in password_history
-    $insertquery_password_history = "INSERT INTO password_history (password, fromdate, is_temp, is_active, use_id) VALUES ('$tmpoldpassword', NOW(), '0', '1', '$tmpuserid');";
-    mysqli_query($db, $insertquery_password_history);
-    //insert old pw in user
-    $insertquery_user = "UPDATE user SET password = '$tmpoldpassword' WHERE use_id = '$tmpuserid';";
-    mysqli_query($db, $insertquery_user);
-
-
-    //send mail for test
-    $to_email = "julian.pichler4@gmail.com";
-    $subject = "We changed you pw back!";
-    $body = "Sheeeh wieder altes pw in DB!";
-    $headers = "From: myarchery.bslinz2@gmail.com";
-    
-    if (mail($to_email, $subject, $body, $headers)) {
-        echo "Email successfully sent to $to_email..." . '<br>';
-    } else {
-        echo "Email sending failed!" . '<br>';
-    }
-
->>>>>>> parent of 0f1e790 (changed reset pw)
 ?>
