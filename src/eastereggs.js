801309a99601bc78f711d75bf250df6a6bd4b7ae
HaunBa <44608793+HaunBa@users.noreply.github.com>
2022-03-01 15:35:15 +0100

    made changes

--- src/eastereggs.js
+++ src/eastereggs.js
@@ -1,3 +1,4 @@
 function julian() {
     document.getElementById("submit").innerHTML = "Mensch Julian das Tötet Menschen";
+    document.getElementsByClassName("submit-btn")[0].innerHTML = "Mensch Julian das tötet Menschen";
 }
