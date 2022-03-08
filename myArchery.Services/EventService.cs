e12d84ec3a1165ffdf650e3e4b0206bcf3afc71f
HaunBa <44608793+HaunBa@users.noreply.github.com>
2022-03-08 15:07:48 +0100

    added EventService and updated gitignore

---
+++ myArchery.Services/EventService.cs
@@ -0,0 +1,30 @@
+﻿using myArchery.Persistance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myArchery.Services
+{
+    public static class EventService
+    {
+        public static int CreateEvent(Event evt)
+        {            
+            using (myarcheryContext db = new myarcheryContext())
+            {
+                db.Events.Add(evt);
+                return db.SaveChanges();
+            }
+        }
+
+        public static int RemoveEvent(int evtId)
+        {
+            using (myarcheryContext db = new myarcheryContext())
+            {
+                db.Events.Remove(db.Events.First(x => x.EveId == evtId));
+                return db.SaveChanges();
+            }
+        }        
+    }
+}
