95ba1a633fd7feec047a0e9493993ea27c066bb7
JulianPichler1 <julian.pichler@students.bs-linz2.ac.at>
2022-03-10 14:23:17 +0100

    d

--- myArchery.Services/obj/Release/net6.0/myArchery.Services.AssemblyInfo.cs
+++ myArchery.Services/obj/Release/net6.0/myArchery.Services.AssemblyInfo.cs
@@ -11,13 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-myArchery-82858A6B-653E-483B-AE32-E0AD0BCA36A3")]
-[assembly: System.Reflection.AssemblyCompanyAttribute("myArchery")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("myArchery.Services")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("myArchery")]
-[assembly: System.Reflection.AssemblyTitleAttribute("myArchery")]
+[assembly: System.Reflection.AssemblyProductAttribute("myArchery.Services")]
+[assembly: System.Reflection.AssemblyTitleAttribute("myArchery.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Von der MSBuild WriteCodeFragment-Klasse generiert.
--- myArchery/obj/Release/net6.0/myArchery.AssemblyInfo.cs
+++ myArchery/obj/Release/net6.0/myArchery.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-myArchery-82858A6B-653E-483B-AE32-E0AD0BCA36A3")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("myArchery")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("myArchery")]
