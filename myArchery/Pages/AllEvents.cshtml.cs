5b63214b37411b09e1546a4a3927193a3289b0ae
HaunBa <44608793+HaunBa@users.noreply.github.com>
2022-03-11 18:07:12 +0100

    Updated AllEvents

--- myArchery/Pages/AllEvents.cshtml.cs
+++ myArchery/Pages/AllEvents.cshtml.cs
@@ -1,28 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using myArchery.Persistance;
+using myArchery.Persistance.Models;
 
 namespace myArchery.Pages.AllEvents
 {
-    public class IndexModel : PageModel
+    public class AllEventsModel : PageModel
     {
-        public void OnGet()
-        {
-
-        }
-
         private readonly myarcheryContext _context;
 
-        public CurrentEventModel(myarcheryContext context)
+        public AllEventsModel(myarcheryContext context)
         {
             _context = context;
         }
 
-        public IList<Event> Event { get; set; }
+        public IList<Event> Event { get; set; } = new List<Event>();
 
         public async Task OnGetAsync()
         {
             Event = await _context.Events
                 .Include(x => x.Par).ToListAsync();
+        }
+
+        public void OnGet()
+        {
+
         }
     }
 }
