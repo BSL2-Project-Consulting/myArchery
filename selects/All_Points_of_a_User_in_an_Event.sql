fee29db64ff655ab9b0c9f3ae9440fecc71e1434
JulianPichler1 <julian.pichler@students.bs-linz2.ac.at>
2022-03-12 12:03:30 +0100

    added dummys and selects

---
+++ selects/All_Points_of_a_User_in_an_Event.sql
@@ -0,0 +1,14 @@
+use myArchery;
+
+-- all points of a user in an event (you have to set eve_id)
+SELECT 
+	e.name AS 'Event Name',
+    u.username AS 'Username',
+    SUM(p.value) AS 'Points'
+FROM arrow a
+LEFT JOIN event_user_roles eur ON a.evusro_id = eur.evusro_id
+LEFT JOIN points p ON a.poi_id = p.poi_id
+LEFT JOIN user u ON eur.use_id = u.use_id
+LEFT JOIN event e ON eur.eve_id = e.eve_id
+WHERE eur.eve_id = 5
+GROUP BY e.name, u.username;
