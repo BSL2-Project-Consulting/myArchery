2c03344d7f8c1845d08ea1104b4fca4b93222fec
HaunBa <44608793+HaunBa@users.noreply.github.com>
2022-03-22 15:30:01 +0100

    Added new Selects and Inserts

---
+++ myArchery.Persistance/Models/ArrowWithInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myArchery.Persistance.Models
+{
+    public class ArrowWithInfo
+    {
+        public string EventName { get; set; }
+        public string Username { get; set; }
+        public string HitType { get; set; }
+        public int Points { get; set; }
+        public DateTime HitTime { get; set; }
+        public string TargetName { get; set; }
+    }
+}
