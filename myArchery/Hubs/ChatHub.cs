53381e1d826cef908b9bca79a28904cde3444933
HaunBa <44608793+HaunBa@users.noreply.github.com>
2022-03-27 13:41:27 +0200

    Updated ChatWs to support groups

--- myArchery/Hubs/ChatHub.cs
+++ myArchery/Hubs/ChatHub.cs
@@ -4,10 +4,19 @@ namespace myArchery.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string name, string message)
+        public async Task SendMessage(int eventId, string name, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", name, message);
+            await Clients.Group(eventId.ToString()).SendAsync("ReceiveMessage", name, message);
             Console.WriteLine("Message Sent");
+        }
+        public async Task AddToGroup(int eventId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, eventId.ToString());
+        }
+
+        public async Task RemoveFromGroup(int eventId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, eventId.ToString());
         }
     }
 }
