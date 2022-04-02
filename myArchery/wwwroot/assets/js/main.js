0af513c34b5992459304c122f859b18e1151b43c
Heinzlmaier Fabian <F.Heinzlmaier@scheuch.com>
2022-04-02 18:51:45 +0200

    added progressive html layout

---
+++ myArchery/wwwroot/assets/js/main.js
@@ -0,0 +1,18 @@
+  /**
+   * Mobile nav toggle
+   */
+   on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
