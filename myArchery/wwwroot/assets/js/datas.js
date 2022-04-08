509de3eab3989c25fabf1d8d9013237fe94836ac
danikabani <100569400+danikabani@users.noreply.github.com>
2022-04-08 09:56:03 +0200

    Create und Join Button

---
+++ myArchery/wwwroot/assets/js/datas.js
@@ -0,0 +1,13 @@
+﻿
+[
+    [1, "Nihilum", "United States", "Hydramist", "Reckful", "33", "2390"],
+    [2, "SK Gaming", "United Kingdom", "Ninja", "SmashBr0", "32", "2243"],
+    [3, "Fnatic", "Sweden", "Snackbar", "Gamer1337", "33", "2121"],
+    [4, "Dignitas", "Finland", "Swifty", "allan-snackbar", "30", "2108"],
+    [5, "Phase", "Austria", "Mercader", "Athenelol", "28", "2048"],
+    [6, "Nihilum", "United States", "Athelete", "Shad0w", "24", "2390"],
+    [7, "SK Gaming", "United Kingdom", "eNergY", "Kungen", "23", "2243"],
+    [8, "Fnatic", "Sweden", "Shwahla", "Elitist", "26", "2121"],
+    [9, "Dignitas", "Finland", "Ghostcrawler", "Metzen", "19", "2108"],
+    [10, "Phase", "Austria", "RMP", "SpawN", "17", "2048"]
+]
