6a8c6b3c8b7ca2106aba3c16d026a3bc4b196f25
JulianPichler1 <julian.pichler@students.bs-linz2.ac.at>
2022-04-09 07:26:39 +0200

    rework curent event

---
+++ html/Git_projekt/src/liveranking.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="liveranking.js"></script>
+    <title>Live Ranking</title>
+</head>
+<body>
+  
+
+
+    <table>
+        <tr>
+          <th></th>
+          <th>Name</th>
+          <th>Tier1</th>
+          <th>Tier2</th>
+          <th>Tier3</th>
+          <th>Zwischenpunkte</th>
+        </tr>
+        <tr>
+          <td>Rank1</td>
+          <td>Spieler2</td>
+          <td>1</td>
+          <td>3</td>
+          <td>2</td>
+         </tr>
+         <tr>
+           <td>Rank2</td>
+           <td>
+             
+             
+          </td>
+           <td>2</td>
+           <td>1</td>
+           <td>3</td>
+         </tr>
+         <tr>
+            <td>Rank3</td>
+            <td></td>
+            <td>3</td>
+            <td>2</td>
+            <td>1</td>
+          </tr>
+      </table>
+      <script>
+         alert("");
+          var js_variable = <?php echo $user_check_query; ?>;
+          // Test:
+         
+          alert(js_variable);
+      </script>  
+
+             <?php
+             
+             session_start();
+             $db = mysqli_connect('localhost', 'root', 'test1234', 'myarchery') or die("could't connect to database");
+ 
+             $user_check_query = "SELECT username FROM user;";
+            
+ 
+             ?>
+</body>
+
+
+</html>
+
+ 
